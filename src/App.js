d00a1083adffdbaef9fab6b13aa31637c4a9395c
nikk14 <neeleshkhandey1818@gmail.com>
2023-01-15 22:19:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import "./App.css";
 import Table from "./component/table";
 
 function App() {
-  // const [name, setName] = useState("this is aman");
-
-  // const handleNameChange = () => {
-  //   setName("thankyou");
-  // };
+ 
 
   return (
     <div className="App">
-      {/* <h2>{name}</h2>
-      <button className="btn" onClick={handleNameChange}>
-        click
-      </button> */}
-
       <Table />
     </div>
   );
